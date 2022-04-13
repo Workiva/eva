31244a0316983ee5d011d3f00a8fed7c8aecc910
Ethan McCue <ethan@mccue.dev>
2022-04-12 22:25:18 -0400

    [Revive]: Clean up eva.v2.fressian

--- core/src/eva/v2/fressian.clj
+++ core/src/eva/v2/fressian.clj
@@ -16,11 +16,13 @@
   "Provides consolidated write & read handlers for use in various circumstances
    depending on the content being handled."
   (:require [eva.v2.storage.value-store.handlers :refer [merge-write-handlers
-                                                      merge-read-handlers]]
+                                                         merge-read-handlers]]
             [recide.sanex.logging :as log]
             [eva.bytes :refer [bba-write-handler bba-read-handler]]
             [eva.datom :refer [datom-write-handler datom-read-handler]]
-            [eva.v2.datastructures.bbtree.fressian :as bbtree]))
+            [eva.v2.datastructures.bbtree.fressian :as bbtree])
+  (:import (org.fressian.handlers WriteHandler ReadHandler)
+           (org.fressian Writer Reader)))
 
 (def eva-only-write-handlers (merge bba-write-handler
                                     bbtree/all-writers
@@ -33,17 +35,17 @@
 (defrecord UnhandledObject [type])
 
 (def object-writer
-  (reify org.fressian.handlers.WriteHandler
+  (reify WriteHandler
     (write [_  writer v]
       (log/warn "unhandled fressian Object writer being activated for:" (type v))
-      (.writeTag ^org.fressian.Writer writer "unhandled-object" 1)
+      (.writeTag ^Writer writer "unhandled-object" 1)
       (.writeString writer (str (type v))))))
 
 (def object-reader
-  (reify org.fressian.handlers.ReadHandler
-    (read [_ reader tag component-count]
+  (reify ReadHandler
+    (read [_ reader _ _]
       (log/warn "unhandled fressian Object reader being activated.")
-      (UnhandledObject. (.readObject ^org.fressian.Reader reader)))))
+      (UnhandledObject. (.readObject ^Reader reader)))))
 
 (def eva-write-handlers (merge-write-handlers
                          (merge bba-write-handler
